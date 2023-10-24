22b63005e114909448be1beb7cd53e569964c861
Dylan Burnham <dburnham1212@gmail.com>
2023-10-23 21:32:36 -0400

    Added sample data and small quality of life upgrades

--- server/db/seeds/00_USERS.sql
+++ server/db/seeds/00_USERS.sql
@@ -1,2 +1,13 @@
-INSERT INTO users (first_name, last_name, phone_number, email, password, role) VALUES ('Dylan', 'Burnham', '289-356-5266', 'dburnham1212@gmail.com', '$2a$12$vwfiBALtJBr07uHNM0y9UeRmOg.d4A4o0Mfqi0g3vxpZcFyQqPUly', 'admin');
-INSERT INTO users (first_name, last_name, phone_number, email, password, role) VALUES ('Test', 'Tester', '555-555-5555', 'test@gmail.com', '$2a$12$vwfiBALtJBr07uHNM0y9UeRmOg.d4A4o0Mfqi0g3vxpZcFyQqPUly', 'user');
+-- Insert base users into the "users" table (password: letmein)
+
+-- User 1
+INSERT INTO users (first_name, last_name, phone_number, email, password, role) 
+VALUES ('Dylan', 'Burnham', '555-555-5555', 'dburnham1212@gmail.com', '$2a$12$vwfiBALtJBr07uHNM0y9UeRmOg.d4A4o0Mfqi0g3vxpZcFyQqPUly', 'admin');
+
+-- User 2
+INSERT INTO users (first_name, last_name, phone_number, email, password, role)
+VALUES ('Test', 'Tester', '555-555-5555', 'test@gmail.com', '$2a$12$vwfiBALtJBr07uHNM0y9UeRmOg.d4A4o0Mfqi0g3vxpZcFyQqPUly', 'user');
+
+-- User 3
+INSERT INTO users (first_name, last_name, phone_number, email, password, role)
+VALUES ('Admin', 'Two', '555-555-5555', 'test2@gmail.com', '$2a$12$vwfiBALtJBr07uHNM0y9UeRmOg.d4A4o0Mfqi0g3vxpZcFyQqPUly', 'admin');
--- server/db/seeds/01_LISTINGS.sql
+++ server/db/seeds/01_LISTINGS.sql
@@ -1,2 +1,49 @@
-INSERT INTO listings (user_id, image_url, name, description, address, price) VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_1.jpeg?raw=true', 'Small Cabin', 'A small cabin in the woods', '39 Milton Street, Oshawa, L1E1Z9', '100');
-INSERT INTO listings (user_id, image_url, name, description, address, price) VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_2.jpg?raw=true', 'Smaller Cabin', 'A smaller cabin in the woods', '39 Fake Street, Oshawa, L1E1Z9', '120');
+-- Insert sample data into the "listings" table
+
+-- Listing 1
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_1.jpg?raw=true', 'Cozy Cabin Retreat', 'A cozy cabin nestled in a peaceful forest setting.', '123 Pine Tree Lane, Toronto, M5V 2H1', 150);
+
+-- Listing 2
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_6.jpg?raw=true', 'Riverside Getaway', 'A charming cabin by the river, perfect for a quiet retreat.', '456 Riverbank Road, Vancouver, V6G 1B2', 250);
+
+-- Listing 3
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_8.jpg?raw=true', 'Wilderness Cabin Hideout', 'A rustic cabin deep in the Canadian wilderness.', '789 Forest Path, Montreal, H2Y 2A7', 180);
+
+-- Listing 4
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_12.jpg?raw=true', 'Ski Chalet in Whistler', 'A cozy ski chalet in the famous Whistler resort area.', '101 Mountain View Road, Whistler, V8E 1M2', 300);
+
+-- Listing 5
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_10.jpg?raw=true', 'Lakeside Getaway', 'A lakeside cabin with breathtaking views of the water.', '222 Lakefront Lane, Victoria, V9A 1B3', 400);
+
+-- Listing 6
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_5.jpg?raw=true', 'Mountain Cabin Retreat', 'A mountain cabin with panoramic views in Calgary.', '333 Summit Trail, Calgary, T2P 3K6', 280);
+
+-- Listing 7
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_4.jpg?raw=true', 'Rustic Lodge in Banff', 'A rustic cabin in the scenic Banff National Park.', '444 Forest Trail, Banff, T1L 1A9', 120);
+
+-- Listing 8
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_3.jpg?raw=true', 'Woodland Getaway', 'A charming cabin in the heart of the Ottawa woodlands.', '555 Pine Grove Drive, Ottawa, K1P 5E2', 190);
+
+-- Listing 9
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_9.jpg?raw=true', 'Historic Cabin in Quebec City', 'A historic cabin in the charming Old Quebec City.', '666 Rue de l''Histoire, Quebec City, G1R 3N3', 350);
+
+-- Listing 10
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_7.jpg?raw=true', 'Seaside Cabin Retreat in Halifax', 'A serene seaside cabin retreat with access to the ocean.', '777 Ocean View Drive, Halifax, B3H 1P9', 220);
+
+-- Listing 11
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (3, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_2.jpg?raw=true', 'Mountain View Cabin', 'A cabin with breathtaking mountain views in Jasper National Park.', '888 Scenic Road, Jasper, T0E 1E0', 160);
+
+-- Listing 12
+INSERT INTO listings (user_id, image_url, name, description, address, price)
+VALUES (1, 'https://github.com/dburnham1212/airbnb/blob/main/images/cabin_11.jpg?raw=true', 'Riverside Cabin Getaway', 'A cozy riverside cabin perfect for relaxation in Edmonton.', '999 Riverfront Drive, Edmonton, T5J 0R2', 100);
